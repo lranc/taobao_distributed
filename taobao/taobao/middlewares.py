d90eb836ee08a232b80efd595c01b7ae4361396a
lranc <807106749@qq.com>
2018-08-01 19:22:51 +0800

    add proxymiddleware, need more cookies

--- taobao/taobao/middlewares.py
+++ taobao/taobao/middlewares.py
@@ -46,3 +46,32 @@ class CookiesMiddleware():
         )
 
 
+class ProxyMiddleware():
+    def __init__(self, proxy_url):
+        self.logger = logging.getLogger(__name__)
+        self.proxy_url = proxy_url
+    
+    def get_random_proxy(self):
+        try:
+            response = requests.get(self.proxy_url)
+            if response.status_code == 200:
+                proxy = response.text
+                return proxy
+        except requests.ConnectionError:
+            return False
+    
+    # 当无法连接时,使用代理尝试
+    def process_request(self, request, spider):
+        if request.meta.get('retry_times'):
+            proxy = self.get_random_proxy()
+            if proxy:
+                uri = 'https://{proxy}'.format(proxy=proxy)
+                self.logger.debug('使用代理 ' + proxy)
+                request.meta['proxy'] = uri
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        settings = crawler.settings
+        return cls(
+            proxy_url=settings.get('PROXY_URL')
+        )
